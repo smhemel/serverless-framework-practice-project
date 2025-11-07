33ac65ceaadf24c81f1fd1a78d66e1514f805ef1
S M HEMEL <smhemel.eu@gmail.com>
2025-11-07 23:45:47 +0600

    implement sign in functionality

---
+++ authService/handlers/signIn.js
@@ -0,0 +1,41 @@
+const { CognitoIdentityProviderClient, InitiateAuthCommand } = require("@aws-sdk/client-cognito-identity-provider");
+
+const client = new CognitoIdentityProviderClient({
+    region: 'ap-south-1', //Specify your AWS region
+});
+
+//Define Cognito App Client ID for user pool authentication
+const CLIENT_ID = "40t596ot83gjo66aupnpgrghl6";
+
+exports.signIn = async (event) => {
+    const { email, password } = JSON.parse(event.body);
+
+    const params = {
+        ClientId: CLIENT_ID,
+        AuthFlow: 'USER_PASSWORD_AUTH', //Auth flow  for username/password
+        AuthParameters: {
+            USERNAME: email,
+            PASSWORD: password,
+        },
+    };
+
+    try {
+        const command = new InitiateAuthCommand(params);
+        const response = await client.send(command);
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                msg: "User successfully signed in!",
+                tokens: response.AuthenticationResult, //this will contain  the 
+                //AccessToken , RefreshToken, and idToken     
+
+            }),
+        };
+
+    } catch (error) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ msg: 'sign-in failed', error: error.message }),
+        };
+    }
+}
