4306d870c7ea9d7f3e55923eb0f27df449769a2c
S M HEMEL <smhemel.eu@gmail.com>
2025-11-08 00:51:48 +0600

    Implement sign out functionality

---
+++ authService/handlers/signOut.js
@@ -0,0 +1,28 @@
+const { CognitoIdentityProviderClient, GlobalSignOutCommand } = require("@aws-sdk/client-cognito-identity-provider");
+
+const client = new CognitoIdentityProviderClient({
+    region: "ap-south-1", //Specify your AWS region
+});
+
+exports.signOut = async (event) => {
+    const { accessToken } = JSON.parse(event.body);
+
+    const params = {
+        AccessToken: accessToken, //The access token from the sign in response
+    };
+
+    try {
+        const command = new GlobalSignOutCommand(params);
+        await client.send(command);
+        
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ msg: "User succesfully signed out!" }),
+        };
+    } catch (error) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ msg: 'sign-out failed', error: error.message }),
+        };
+    }
+}
