51dfd72829dac53583750abd24a069dbb5d33099
S M HEMEL <smhemel.eu@gmail.com>
2025-11-09 01:52:21 +0600

    Implement SNS for cleanup caterogries

--- categoryService/handlers/cleanupCategories.js
+++ categoryService/handlers/cleanupCategories.js
@@ -1,12 +1,15 @@
 const { DynamoDBClient, ScanCommand, DeleteItemCommand } = require('@aws-sdk/client-dynamodb');
+const { SNSClient, PublishCommand } = require('@aws-sdk/client-sns');
 
 const dynamoDbClient = new DynamoDBClient({ region: "ap-south-1" });
+const snsClient = new SNSClient({ region: "ap-south-1" });
 
 //Define  the cleanup function to remove outdated categories
 exports.cleanupCategories = async () => {
     try {
         //Get the Dynamodb table name from the enviroment varaibles
-        const tableName = process.env.DYNAMODB_TABLE;
+        const tableName = process.env.DYNAMO_TABLE;
+        const snsTopicArn = process.env.SNS_TOPIC_ARN;
 
         //Calcalate the timestamp for one hour ago(to filter outdated categories)
         const oneHourAgo = new Date(Date.now() - 60 * 60 * 1000).toISOString();
@@ -47,6 +50,14 @@ exports.cleanupCategories = async () => {
             deletedCount++; //Increament the count of deleted items
         }
 
+        //send an SNS notication after deleting categories
+        const snsMessage = `Cleanup completed. Deleted ${deletedCount} outdated categories`;
+
+        await snsClient.send(new PublishCommand({
+            TopicArn: snsTopicArn,
+            Message: snsMessage,
+            Subject: "Category cleanup Notification",
+        }));
         //return a success response with the total number of deleted  categories
 
         return {
